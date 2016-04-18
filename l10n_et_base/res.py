cf638bf643adf4f4f8b7d18db0ea31b9f9adc3a3
Mike Telahun <miket@clearict.com>
2016-04-18 22:43:25 +0300

    l10n_et_base: PEP8 fix

--- l10n_et_base/res.py
+++ l10n_et_base/res.py
@@ -1,4 +1,4 @@
-# -*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    Copyright (C) 2016 Clear ICT Solutions <info@clearict.com>.
