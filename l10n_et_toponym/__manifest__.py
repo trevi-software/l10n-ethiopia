eba136aabd54e92b01ce80660f4fd67370bbecbf
Mike Telahun <miket@clearict.com>
2019-08-13 08:51:49 +0300

    [MIG] l10n_et_toponym: Migration to 14.0
    Remove 'development_status' key from manifest file
    
    Format manifest file with black
    
    encoding pragma no longer necessary in python 3.x
    
    Remove encoding pragma and adjust copyright stanza

--- l10n_et_toponym/__manifest__.py
+++ l10n_et_toponym/__manifest__.py
@@ -1,36 +1,18 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2012,2013,2019 Michael Telahun Makonnen <miket@clearict.com>.
-#    All Rights Reserved.
-#
-#    This program is free software: you can redistribute it and/or modify it
-#    under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2021 Michael Telahun Makonnen
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Ethiopia - States and Provinces',
-    'version': '12.0.1.0.0',
-    'category': 'Localization',
-    'license': 'AGPL-3',
-    'author': 'Clear ICT Solutions, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-ethiopia/',
-    'depends': [
-        'base',
+    "name": "Ethiopia - States and Provinces",
+    "version": "14.0.1.0.0",
+    "author": "Clear ICT Solutions, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/l10n-ethiopia",
+    "category": "Localization",
+    "depends": [
+        "base",
     ],
-    'data': [
-        'data/res.country.state.csv',
+    "data": [
+        "data/res.country.state.csv",
     ],
-    'installable': True,
+    "installable": True,
 }
