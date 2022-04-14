1cbf54a4d1404d90ec98d8dea544e2ae6f50bd60
Michael Telahun <mtm@trevi.et>
2022-04-14 22:59:11 +0300

    l10n_et_hr: resolve remaining english/ethiopic name display issues

--- l10n_et_hr/models/hr_job.py
+++ l10n_et_hr/models/hr_job.py
@@ -14,12 +14,14 @@ class HrJob(models.Model):
 
         res = []
         use_ethiopic_name = self.env["ir.config_parameter"].get_param(
-            "l10n_et_hr.use_ethiopic_job"
+            "l10n_et_hr.use_ethiopic_job_name"
         )
-        for rec in self:
-            name = rec.name
-            if use_ethiopic_name:
-                name = rec.ethiopic_name
-            res.append((rec.id, "%s" % (name)))
+        if use_ethiopic_name:
+            for rec in self:
+                name = rec.name
+                if rec.ethiopic_name:
+                    name = rec.ethiopic_name
+                res.append((rec.id, name))
+            return res
 
-        return res
+        return super().name_get()
