0204622f378a44e2094ca3c136adb31d9b13c138
Michael Telahun <mtm@trevi.et>
2022-09-09 09:37:00 +0300

    black, isort, flake

--- l10n_et_hr_salary_structure_type/models/hr_contract_type.py
+++ l10n_et_hr_salary_structure_type/models/hr_contract_type.py
@@ -5,6 +5,6 @@ from odoo import fields, models
 
 
 class HrPayrollStructureType(models.Model):
-    _inherit = 'hr.payroll.structure.type'
+    _inherit = "hr.payroll.structure.type"
 
     active = fields.Boolean(default=True)
