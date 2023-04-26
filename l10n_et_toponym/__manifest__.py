915481e3a001dafbb2a7ab42d4929258de01bf64
Michael Telahun <mtm@trevi.et>
2023-04-26 10:54:29 +0300

    Add 2 new regional states: SD and SW

--- l10n_et_toponym/__manifest__.py
+++ l10n_et_toponym/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2021 Michael Telahun Makonnen
+# Copyright 2021,2023 Michael Telahun Makonnen
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Ethiopia - States and Provinces",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.2",
     "author": "Clear ICT Solutions, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/l10n-ethiopia",
@@ -12,7 +12,7 @@
         "base",
     ],
     "data": [
-        "data/res.country.state.csv",
+        "data/regional_states.xml",
     ],
     "installable": True,
 }
