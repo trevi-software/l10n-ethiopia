23c25e5945529697fdd9a1ca3653e69d47a1cf63
Michael Telahun Makonnen <mtm@trevi.et>
2026-06-22 11:55:00 +0300

    [MIG][15.0] pre-commit, black, etc

--- l10n_et_tz/__manifest__.py
+++ l10n_et_tz/__manifest__.py
@@ -9,7 +9,7 @@
     "images": ["static/src/img/main_screenshot.png"],
     "license": "AGPL-3",
     "author": "TREVI Software, Michael Telahun Makonnen",
-    "website": "https://github.com/trevi-software/trevi-hr",
+    "website": "https://github.com/OCA/l10n-ethiopia",
     "depends": [
         "base",
     ],
