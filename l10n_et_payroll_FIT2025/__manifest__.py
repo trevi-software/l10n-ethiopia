213fad0a75868d14185c47333925492b1d20968a
Michael Telahun Makonnen <mtm@trevi.et>
2026-06-22 11:55:00 +0300

    [MIG][15.0] pre-commit, black, etc

--- l10n_et_payroll_FIT2025/__manifest__.py
+++ l10n_et_payroll_FIT2025/__manifest__.py
@@ -10,7 +10,7 @@
     "images": ["static/src/img/main_screenshot.png"],
     "license": "AGPL-3",
     "author": "TREVI Software",
-    "website": "https://github.com/trevi-software/trevi-hr",
+    "website": "https://github.com/OCA/l10n-ethiopia",
     "depends": [
         "payroll",
         "l10n_et_payroll_category",
