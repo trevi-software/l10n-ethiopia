5862664f3b021bfa5e347dedfd9bd9cd19eaa7e8
Michael Telahun Makonnen <mtm@trevi.et>
2026-06-25 14:35:40 +0300

    [MIG] Migrate all modules to Odoo 16.0

--- l10n_et_tz/__manifest__.py
+++ l10n_et_tz/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Ethiopia - Timezone",
     "summary": "Set default timezone to Africa/Addis_Ababa",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "images": ["static/src/img/main_screenshot.png"],
     "license": "AGPL-3",
     "author": "TREVI Software, Michael Telahun Makonnen",
