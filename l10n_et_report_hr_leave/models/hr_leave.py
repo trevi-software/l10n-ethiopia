af575d1234b9207c743f5edf2cd28e989a5ef693
Michael Telahun Makonnen <mtm@trevi.et>
2026-07-24 14:14:28 +0300

    [IMP] l10n_et_report_hr_leave: pre-commit auto fixes

--- l10n_et_report_hr_leave/models/hr_leave.py
+++ l10n_et_report_hr_leave/models/hr_leave.py
@@ -17,15 +17,16 @@ from odoo.addons.ethiopic_calendar.models.ethiopic_calendar import (
 from odoo.addons.ethiopic_calendar.models.pycalcal import pycalcal as pcc
 
 _logger = logging.getLogger(__name__)
-class HrLeave(models.Model):
 
+
+class HrLeave(models.Model):
     _inherit = "hr.leave"
 
     return_date_et = fields.Char("Ethiopic Return Date")
 
-    local_date_from = fields.Datetime("Local Date From", compute="_localize_date_from_to")
+    local_date_from = fields.Datetime(compute="_compute_date_from_to")
 
-    local_date_to = fields.Datetime("Local Date To", compute="_localize_date_from_to")
+    local_date_to = fields.Datetime(compute="_compute_date_from_to")
 
     rest_days = fields.Float(
         "Rest (Days)",
@@ -59,18 +60,18 @@ class HrLeave(models.Model):
     )
 
     available_leave_days = fields.Float(
-        "Available Leave Days",
         compute="_compute_available_leave_days",
         readonly=True,
         tracking=False,
-        help="Number of available leave days for the employee",)
+        help="Number of available leave days for the employee",
+    )
 
     taken_leave_days = fields.Float(
-        "Taken Leave Days",
         compute="_compute_taken_leave_days",
         readonly=True,
         tracking=False,
-        help="Number of available leave days for the employee",)
+        help="Number of available leave days for the employee",
+    )
 
     def _compute_rest_days(self):
         for record in self:
@@ -97,29 +98,32 @@ class HrLeave(models.Model):
     def _compute_taken_leave_days(self):
         leaves = self.env["hr.leave"]
         for record in self:
-            taken_leaves = leaves.search([
-                ("employee_id", "=", record.employee_id.id),
-                ("holiday_status_id", "=", record.holiday_status_id.id),
-                ("state", "=", "validate"),
-            ])
+            taken_leaves = leaves.search(
+                [
+                    ("employee_id", "=", record.employee_id.id),
+                    ("holiday_status_id", "=", record.holiday_status_id.id),
+                    ("state", "=", "validate"),
+                ]
+            )
             record.taken_leave_days = sum(taken_leaves.mapped("number_of_days"))
 
     @api.depends("number_of_days", "taken_leave_days")
     def _compute_available_leave_days(self):
         allocation = self.env["hr.leave.allocation"]
         for record in self:
-            my_allocations = allocation.search([
-                ("employee_id", "=", record.employee_id.id),
-                ("holiday_status_id", "=", record.holiday_status_id.id),
-                ("state", "=", "validate"),
-            ])
+            my_allocations = allocation.search(
+                [
+                    ("employee_id", "=", record.employee_id.id),
+                    ("holiday_status_id", "=", record.holiday_status_id.id),
+                    ("state", "=", "validate"),
+                ]
+            )
             record.available_leave_days = (
                 sum(my_allocations.mapped("number_of_days")) - record.taken_leave_days
             )
 
     @api.model
     def time2ethiopic(self, year, month, day):
-
         # Convert to Ethiopic calendar
         pcc_date = pcc.ethiopic_from_fixed(
             pcc.fixed_from_gregorian(pcc.gregorian_date(year, month, day))
@@ -136,19 +140,17 @@ class HrLeave(models.Model):
 
     @api.onchange("date_to")
     def onchange_enddate(self):
-
         for record in self:
             dt = record.date_to
             if record.date_to:
                 record.return_date_et = record.time2ethiopic(
-                        int(dt.strftime("%Y")),
-                        int(dt.strftime("%m")),
-                        int(dt.strftime("%d")),
-                    )
+                    int(dt.strftime("%Y")),
+                    int(dt.strftime("%m")),
+                    int(dt.strftime("%d")),
+                )
 
     @api.model
     def format_date(self, date_str):
-
         if not date_str:
             return ""
         d = datetime.strptime(date_str, OE_DTFORMAT)
@@ -156,7 +158,6 @@ class HrLeave(models.Model):
 
     @api.model
     def format_date_et(self, date_str):
-
         if not date_str:
             return ""
         d = datetime.strptime(date_str, OE_DTFORMAT)
@@ -164,11 +165,8 @@ class HrLeave(models.Model):
 
     @api.model
     def get_remaining_leaves(self, leave):
-
         obj = self.env["hr.leave.type"]
-        res = obj._get_remaining_leaves(
-            leave.employee_id.id
-        )
+        res = obj._get_remaining_leaves(leave.employee_id.id)
         res = res[leave.employee_id.id]
         if (
             res[leave.holiday_status_id.id].get("max_leaves", False)
@@ -187,11 +185,8 @@ class HrLeave(models.Model):
 
     @api.model
     def get_taken_leaves(self, leave):
-
         obj = self.env["hr.leave.type"]
-        res = obj._get_remaining_leaves(
-            leave.employee_id.id
-        )
+        res = obj._get_remaining_leaves(leave.employee_id.id)
         res = res[leave.employee_id.id]
         if (
             res[leave.holiday_status_id.id].get("max_leaves", False)
@@ -212,7 +207,6 @@ class HrLeave(models.Model):
 
     @api.model
     def get_hrm(self):
-
         hrm_data = ("", "", _("HR Manager"), "የሠው ሃይል አስተዳደር")
         hrm_id = self.env["ir.config_parameter"].sudo().get_param("hr.hr_manager_id")
         if hrm_id:
@@ -226,26 +220,23 @@ class HrLeave(models.Model):
 
         return hrm_data
 
-    @api.depends('date_from', 'date_to')
-    def _localize_date_from_to(self):
-
+    @api.depends("date_from", "date_to")
+    def _compute_date_from_to(self):
         for record in self:
             if record.date_from:
-                record.local_date_from = UTC \
-                                            .localize(record.date_from) \
-                                            .astimezone(timezone(
-                                                record.employee_id.tz or 'UTC'
-                                            )) \
-                                            .replace(tzinfo=None)
+                record.local_date_from = (
+                    UTC.localize(record.date_from)
+                    .astimezone(timezone(record.employee_id.tz or "UTC"))
+                    .replace(tzinfo=None)
+                )
 
             else:
                 record.local_date_from = False
             if record.date_to:
-                record.local_date_to = UTC \
-                                            .localize(record.date_to) \
-                                            .astimezone(timezone(
-                                                record.employee_id.tz or 'UTC'
-                                            )) \
-                                            .replace(tzinfo=None)
+                record.local_date_to = (
+                    UTC.localize(record.date_to)
+                    .astimezone(timezone(record.employee_id.tz or "UTC"))
+                    .replace(tzinfo=None)
+                )
             else:
                 record.local_date_to = False
