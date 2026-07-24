d57367324ce09240fa24cdbe249d9b4ed91e86f0
Michael Telahun Makonnen <mtm@trevi.et>
2026-07-24 14:39:04 +0300

    [IMP] l10n_et_tz: pre-commit auto fixes

--- l10n_et_tz/tests/test_res_users.py
+++ l10n_et_tz/tests/test_res_users.py
@@ -13,14 +13,12 @@ class TestResUsers(common.SavepointCase):
         cls.ResUsers = cls.env["res.users"]
 
     def test_default_tz(self):
-
         john = self.ResUsers.create({"name": "John", "login": "john"})
         self.assertEqual(
             john.tz, "Africa/Addis_Ababa", "By default the user has Ethiopia timezone"
         )
 
     def test_admin_tz(self):
-
         admin = self.env.ref("base.user_admin")
         self.assertEqual(
             admin.tz,
