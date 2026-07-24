75ef84112546de24df474167c1bd8b5662a7969a
Michael Telahun Makonnen <mtm@trevi.et>
2026-07-24 14:32:18 +0300

    [MIG] l10n_et_toponym: Migration to 17.0

--- l10n_et_toponym/__manifest__.py
+++ l10n_et_toponym/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Ethiopia - States and Provinces",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "TREVI Software, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/l10n-ethiopia",
