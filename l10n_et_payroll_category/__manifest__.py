686d7551369c7830f398a30b737d2559e83935a8
Michael Telahun Makonnen <mtm@trevi.et>
2026-07-24 14:02:42 +0300

    [MIG] l10n_et_payroll_category: Migration to 17.0

--- l10n_et_payroll_category/__manifest__.py
+++ l10n_et_payroll_category/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Payroll Categories",
     "summary": "Additional payroll categories for Ethiopia",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Localization",
     "images": ["static/src/img/main_screenshot.png"],
     "license": "AGPL-3",
