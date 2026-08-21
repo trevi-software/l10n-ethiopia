b2b07d5169e69e9dcf6e7f6d94667029aef1b959
Michael Telahun Makonnen <mtm@trevi.et>
2026-08-21 16:40:19 +0300

    [18.0][MIG] l10n_et_payroll_FIT2025: Migrate to 18.0

--- l10n_et_payroll_FIT2025/__manifest__.py
+++ l10n_et_payroll_FIT2025/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Federal Income Tax Tables 2017",
     "summary": "Ethiopian Federal Income Tax tables (rev. 2017)",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Localization",
     "images": ["static/src/img/main_screenshot.png"],
     "license": "AGPL-3",
