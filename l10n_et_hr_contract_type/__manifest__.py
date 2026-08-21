a4df8a61eb9f49cce5b3412f15ef483598a08121
Michael Telahun Makonnen <mtm@trevi.et>
2026-08-21 12:38:22 +0300

    [18.0][MIG] l10n_et_hr_contract_type: Migrate to 18.0

--- l10n_et_hr_contract_type/__manifest__.py
+++ l10n_et_hr_contract_type/__manifest__.py
@@ -4,9 +4,8 @@
 {
     "name": "Contract Types - Ethiopia",
     "summary": "Ethiopian localization of Employee Contract Types",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Localization",
-    "images": ["static/src/img/main_screenshot.png"],
     "license": "AGPL-3",
     "author": "TREVI Software",
     "website": "https://github.com/OCA/l10n-ethiopia",
